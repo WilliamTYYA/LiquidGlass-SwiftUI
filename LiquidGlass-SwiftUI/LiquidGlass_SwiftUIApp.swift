1b3fcec89be64ec8a060f45f555504834e32aa50
Thiha Ye Yint Aung <thihayeyintaung110919@gmail.com>
2025-11-30 18:04:20 -0600

    Initial Commit

---
+++ LiquidGlass-SwiftUI/LiquidGlass_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlass_SwiftUIApp.swift
+//  LiquidGlass-SwiftUI
+//
+//  Created by Thiha Ye Yint Aung on 11/30/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlass_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiquidGlass-SwiftUITests/LiquidGlass_SwiftUITests.swift
@@ -0,0 +1,16 @@
+//
+//  LiquidGlass_SwiftUITests.swift
+//  LiquidGlass-SwiftUITests
+//
+//  Created by Thiha Ye Yint Aung on 11/30/25.
+//
+
+import Testing
+
+struct LiquidGlass_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
